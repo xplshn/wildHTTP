68bf0e1ea0a4f814f11032839fc8627924036e07
xplshn <xplshn@murena.io>
2024-10-13 01:14:11 -0300

    Make good use of my head
    Signed-off-by: xplshn <xplshn@murena.io>

--- serveThePublic.go
+++ serveThePublic.go
@@ -1,27 +1,43 @@
 package main
 
 import (
+	"fmt"
+	"io"
 	"log"
 	"net/http"
 	"os"
 )
 
+func getPublicIP() (string, error) {
+	resp, err := http.Get("https://api.ipify.org")
+	if err != nil {
+		return "", err
+	}
+	defer resp.Body.Close()
+
+	ip, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return "", err
+	}
+
+	return string(ip), nil
+}
+
 func main() {
-	// Define the port for the server, fallback to port 8080
 	port := "8080"
 	if envPort := os.Getenv("PORT"); envPort != "" {
 		port = envPort
 	}
 
-	// Serve files from the ./public directory
+	ip, err := getPublicIP()
+	if err != nil {
+		log.Fatalf("Failed to fetch public IP: %v", err)
+	}
+
+	fmt.Printf("Server is running on public IP: %s, port: %s\n", ip, port)
+
 	http.Handle("/", http.FileServer(http.Dir("./public")))
 
-	// Log the server starting
-	log.Printf("Server starting on port %s\n", port)
-
-	// Start serving
-	err := http.ListenAndServe(":"+port, nil)
-	if err != nil {
-		log.Fatal("Failed to start server: ", err)
-	}
+	log.Printf("Serving files at http://%s:%s\n", ip, port)
+	log.Fatal(http.ListenAndServe(":"+port, nil))
 }
